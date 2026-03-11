561c2de22d1d7769e0cb6332ec4a12c260a7c4c5
rethik777 <rethik.22cs@kct.in>
2026-03-11 17:58:01 +0530

    updated

--- OMA - frontend/vite.config.ts
+++ OMA - frontend/vite.config.ts
@@ -7,7 +7,7 @@
 
 
   export default defineConfig(({ mode }) => {
-    const env = loadEnv('development', process.cwd(), '');
+    const env = loadEnv(mode, process.cwd(), '');
     
     return {
     plugins: [react(), tailwindcss(),svgr()],
